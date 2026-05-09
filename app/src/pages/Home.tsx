5641965c34e2aae94dd98f13333981fca18540c4
Gustavo Gonçalves <e1164@itsector.pt>
2026-05-09 16:34:45 +0100

    update design

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -202,7 +202,7 @@ export default function Home() {
       {/* Bottom carousel */}
       <section className="absolute bottom-0 left-0 right-0 z-30 pb-20 md:pb-12 pt-20 px-4 md:px-12 bg-linear-to-t from-surface via-surface/90 to-transparent">
         <div className="max-w-7xl mx-auto">
-          <LocaleExampleCard />
+          {/* <LocaleExampleCard /> */}
 
           <div className="flex justify-between items-end mb-4 md:mb-6 px-1 md:px-4">
             <div>
